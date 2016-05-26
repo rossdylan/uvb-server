9d458032e8e485294c32da7ba17e80400f4830b4
Ross Delinger <rossdylan@csh.rit.edu>
2016-05-26 14:40:50 -0400

    Finished LMDB counter implementation, uses MDB_WRITEMAP and MDB_MAPSYNC to defer msync calls in order to improve speed, only time will tell if this is safe enough

--- include/lmdb_counter.h
+++ include/lmdb_counter.h
@@ -2,6 +2,7 @@
 
 #include <lmdb.h>
 #include <stdint.h>
+#include "buffer.h"
 
 typedef struct {
     MDB_env *env;
@@ -13,3 +14,5 @@ lmdb_counter_t *lmdb_counter_init(const char *path, uint64_t readers);
 void lmdb_counter_destroy(lmdb_counter_t *lc);
 uint64_t lmdb_counter_inc(lmdb_counter_t *lc, const char *key);
 uint64_t lmdb_counter_get(lmdb_counter_t *lc, const char *key);
+void lmdb_counter_sync(lmdb_counter_t *lc);
+void lmdb_counter_dump(lmdb_counter_t *lc, buffer_t *buffer);
